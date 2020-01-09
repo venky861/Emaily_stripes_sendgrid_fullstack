e31abd30adbdb55a1fd9f7cc606de541cc01dfbb
venky861 <venkisekaran77@gmail.com>
2020-01-09 14:34:31 +0530

    hey

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    "689673227100-6rvhl3ciu3brjktpui94ug9jsq5b31sf.apps.googleusercontent.com",
+  googleClientSecret: "t2p7mnQpOxTl_UEU0ZwvVNfb",
+  mongoURI:
+    "mongodb+srv://Venki407:Venkitheviper3@cluster0-vo3nx.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey:
+    "sjfjsanfjafuefuecnmsanfsafnjahfawfuanfasjfnjsanfakjsnfieafiefnnsfnms"
+}
